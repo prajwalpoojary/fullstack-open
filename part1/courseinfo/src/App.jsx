db090c9b304459caa32d6a31c9f7ca6b3e12f207
Prajwal Poojary <prajwalpoojary67@gmail.com>
2024-04-28 00:47:37 +0530

    part1 exercise 1.2 step 2 - refactor content component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,6 +6,16 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part={props.part.part1} exercise={props.exercise.exercises1}  />
+      <Part part={props.part.part2} exercise={props.exercise.exercises2}  />
+      <Part part={props.part.part3} exercise={props.exercise.exercises3}  />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
     <p>{props.part} {props.exercise}</p>
   )
 }
@@ -28,9 +38,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part={{part1, part2, part3}} exercise={{exercises1, exercises2, exercises3}} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
